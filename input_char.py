5ff7fb926a471e452a6db46f0473032c31ea8847
ramarao4507 <ramarao4507@gmail.com>
2017-11-28 08:57:41 +0530

    Add files via upload

---
+++ input_char.py
@@ -0,0 +1,13 @@
+i = input("enter string is ")
+print "input string is " + i
+
+
+#Manuplating strings :
+
+age = int(input("enter age in str format"))
+print  age
+print type(age)
+
+
+
+print str(4) + "name"
